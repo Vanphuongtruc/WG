8de9838b03951dd328f1d0c6cd0fbddaab023550
ainzworth <56994003+ainzworth@users.noreply.github.com>
2020-05-24 12:57:16 +0700

    Update reciept.php

--- reciept.php
+++ reciept.php
@@ -1,10 +1,11 @@
 <?php
-include "tools.php";
 session_start();
+include "tools.php";
 
 
 
 
+// check if the input data is valid 
 $error = false;
 $required = array('username', 'email', 'mobile', 'creditcard');
 foreach($required as $field) {
@@ -13,10 +14,20 @@ foreach($required as $field) {
     $error = true;
   }
 }
-
 if ($error) {
   header('location: index.php');
 } 
+// write validate input into booking.txt
+$myfile = fopen("bookings.txt","a");
+$cells = array_merge(
+    (array) $_SESSION["username"],
+    (array) $_SESSION["email"],
+    (array) $_SESSION["mobile"],
+    (array) $_SESSION["creditcard"],
+    (array) $_SESSION["dateTo"]
+);
+fputcsv($myfile, $cells);
+fclose($myfile);
 preShow($_SESSION);
-echo $_SESSION['username'];
+
 ?>
